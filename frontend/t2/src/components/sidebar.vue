eab1fc8f48c3d0ec3bbcf8c85aadfecaf4bd4001
Minjae <minjae.jung0@gmail.com>
2020-07-31 21:47:40 -0400

    update

--- frontend/t2/src/components/sidebar.vue
+++ frontend/t2/src/components/sidebar.vue
@@ -2,9 +2,14 @@
   <div class="sidebar-wrapper bg-light border-right border-top">
       <div v-if="queueData" id="sidebarContainer" class="friendList">
           <div v-for="(user, index) in queueData.users" :key="index" class="friendTile bg-light">
-              {{ user }}
-          </div>
-      </div>
+              <div class='icon-container'>
+                <img :src="user.avi"/>
+                <div :class='checkStatus(user)'>
+                </div>
+            </div>
+            <p> {{ user.status }} </p>
+          </div> 
+      </div> 
   </div>
 </template>
 
@@ -14,6 +19,15 @@ export default {
     data() {
         return {
 
+        }
+    },
+    methods:{
+        checkStatus(user){
+            if(user.status.toLowerCase() === "online"){
+                return "status-online"
+            }else{
+                return "status-busy"
+            }
         }
     },
     computed: {
@@ -41,6 +55,62 @@ export default {
     margin-bottom: 0;
 }
 
+
+.friendTile img{
+  float: left;
+  max-width: 60px;
+  width: 100%;
+  height: 60px;
+  margin-right: 20px;
+  border-radius: 50%;
+  max-height: 60px;
+  overflow: hidden;
+  width: auto;
+
+}
+
+.icon-container {
+  width: 60px;
+  height: 60px;
+  position: relative;
+}
+
+.status-circle {
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: grey;
+  border: 2px solid white;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+}
+
+.status-online {
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: green;
+  border: 2px solid white;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+}
+.status-busy {
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: yellow;
+  border: 2px solid white;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+}
+
+.friendTile{
+    display: inline;
+
+}
 /* .friendTile .last {
 
 } */
